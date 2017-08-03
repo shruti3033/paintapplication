2449be103c3d8b9b817a5da683aff83179f896d7
shruti3033 <meda2340@gmail.com>
2017-08-03 00:05:44 -0400

    adding images through service

--- src/js/paint.js
+++ src/js/paint.js
@@ -1,7 +1,7 @@
-var module = angular.module('app.paint.controller',[]);
+var module = angular.module('app.paint.controller',['app.paint.service']);
 
-	module.$inject = ['$scope'];
-	module.controller('paintController',function($scope){
+	module.$inject = ['$scope','imageService'];
+	module.controller('paintController',function($scope, imageService){
 		$scope.showFlip = [];
 		$scope.flip = function(img){
 			debugger;
@@ -9,14 +9,7 @@ var module = angular.module('app.paint.controller',[]);
 			$scope.time = time;
 			$scope.showFlip[img.id] = true;
 		}
-		$scope.images =[
-			{"id" : 'src/resources/1.jpg',"title" : "Together" ,"price": 100 , "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"id" : 'src/resources/2.jpg',"title" : "Towards the Sunset", "price" : 110,  "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"id" : 'src/resources/3.jpg',"title" : "Beautiful Fall","price" :210,  "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"id" : 'src/resources/4.jpg',"title" : "Dreamland with my Dreams", "price" : 310,  "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"id" : 'src/resources/5.jpg',"title" : "Nature", "price" : 410, "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-			{"id" : 'src/resources/6.jpg',"title" : "Art of Village", "price" : 510,  "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-		]
+		$scope.images = imageService.getImage();
 		$scope.flipOnce = function(img){
 			$scope.showFlip[img.id] = false;	
 		}
